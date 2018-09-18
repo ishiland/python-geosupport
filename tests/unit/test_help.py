e2e64883d4abbd70aa6a167e00db04903d164a54
Jeremy Neiman <jneiman@dsny.nyc.gov>
2018-09-18 12:54:14 -0400

    fix help unit tests

--- tests/unit/test_help.py
+++ tests/unit/test_help.py
@@ -1,15 +1,18 @@
+import io
+import sys
+
 from ..testcase import TestCase
 
 class TestHelp(TestCase):
 
     def test_get_function_list(self):
-        h = self.geosupport.help()
+        h = self.geosupport.help(return_as_string=True)
 
         self.assertTrue("1" in h)
         self.assertTrue("bbl" in h)
 
     def test_function_help(self):
-        h = self.geosupport.help("bbl")
+        h = self.geosupport.help("bbl", return_as_string=True)
 
         self.assertTrue("Function BL processes" in h)
         self.assertTrue("Input: Borough" in h)
@@ -18,6 +21,17 @@ class TestHelp(TestCase):
         self.assertTrue("Mode Switch" in h)
 
     def test_invalid_function_help(self):
-        h = self.geosupport.help("fake")
+        h = self.geosupport.help("fake", return_as_string=True)
 
         self.assertEqual(h, "Function 'fake' does not exist.")
+
+    def test_print(self):
+        h = io.StringIO()
+        sys.stdout = h
+
+        o = self.geosupport.help()
+
+        sys.stdout = sys.__stdout__
+
+        self.assertTrue("bbl" in h.getvalue())
+        self.assertTrue(o is None)

e8d4a855bb1514bf706fa69090c3e0698fcca603
Ian Shiland <ishiland@gmail.com>
2019-08-23 10:33:14 -0400

    add fips to borough dict. Fix #19.

--- geosupport/config.py
+++ geosupport/config.py
@@ -10,11 +10,11 @@ FUNCTION_INPUTS_CSV = path.join(FUNCTION_INFO_PATH, 'function_inputs.csv')
 WORK_AREA_LAYOUTS_PATH = path.join(FUNCTION_INFO_PATH, 'work_area_layouts')
 
 BOROUGHS = {
-    'MANHATTAN': 1, 'MN': 1, 'NEW YORK': 1, 'NY': 1,
-    'BRONX': 2, 'THE BRONX': 2, 'BX': 2,
-    'BROOKLYN': 3, 'BK': 3, 'BKLYN': 3, 'KINGS': 3,
-    'QUEENS': 4, 'QN': 4, 'QU': 4,
-    'STATEN ISLAND': 5, 'SI': 5, 'STATEN IS': 5, 'RICHMOND': 5,
+    'MANHATTAN': 1, 'MN': 1, 'NEW YORK': 1, 'NY': 1, '36061': 1,
+    'BRONX': 2, 'THE BRONX': 2, 'BX': 2, '36005': 2,
+    'BROOKLYN': 3, 'BK': 3, 'BKLYN': 3, 'KINGS': 3, '36047': 3,
+    'QUEENS': 4, 'QN': 4, 'QU': 4, '36081': 4,
+    'STATEN ISLAND': 5, 'SI': 5, 'STATEN IS': 5, 'RICHMOND': 5, '36085': 5,
     '': '',
 }
 
